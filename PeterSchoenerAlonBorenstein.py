4e8eb5f77d0529f408510d99a6ba1068c3fc2e68
Alon Borenstein <Alon Borenstein>
2017-06-02 13:13:48 +0200

    init commit

---
+++ PeterSchoenerAlonBorenstein.py
@@ -0,0 +1,11 @@
+import nltk
+#--------------Ex1------------------
+def tokenize(path):
+    sentences=[]
+    words=[]
+    for line in open(path,"r",encoding="utf-8"):
+        sentences.append(nltk.sent_tokenize(line))
+        #for word in line:
+        #    words.append(nltk.word_tokenize(
+    print(sentences)
+tokenize("C:/Users/alon464/Desktop/assignment1-data/c00.txt")
