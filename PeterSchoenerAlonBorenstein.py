0850d8598d1cea085ecb433a333b97a9391d4943
Alon Borenstein <Alon Borenstein>
2017-06-02 13:26:47 +0200

    word tokenizer

--- PeterSchoenerAlonBorenstein.py
+++ PeterSchoenerAlonBorenstein.py
@@ -5,8 +5,8 @@ def tokenize(path):
     words=[]
     for line in open(path,"r",encoding="utf-8"):
         sentences.append(nltk.sent_tokenize(line))
-        #for word in line:
-        #    words.append(nltk.word_tokenize(
+        for word in line:
+            words.append(nltk.word_tokenize(line))
     print(sentences)
 tokenize("C:/Users/alon464/Desktop/assignment1-data/c00.txt")
 print("hi Alon")
