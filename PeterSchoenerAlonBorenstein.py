be479be6fcc122e99b84a3f3be84d1316a288770
Alon Borenstein <Alon Borenstein>
2017-06-06 12:53:58 +0200

    read file

--- PeterSchoenerAlonBorenstein.py
+++ PeterSchoenerAlonBorenstein.py
@@ -9,10 +9,12 @@ import nltk
 def tokenize(path):
     sentences=[]
     words=[]
-    for line in open(path,"r",encoding="utf-8"):
-        sentences.append(nltk.sent_tokenize(line))
-        for word in line:
-            words.append(nltk.word_tokenize(line))
-    print(sentences)
+    file=open(path,"r",encoding="utf-8")
+    text=file.read().replace("\n", " ")
+    sentences = nltk.sent_tokenize(text)
+    words = []
+    for sentence in sentences :
+        words.append(nltk.word_tokenize(sentence))
+    print(words)
 tokenize("C:/Users/alon464/Desktop/assignment1-data/c00.txt")
 print("hi Alon")
