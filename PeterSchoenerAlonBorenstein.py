6f8e7a0c7cefbf0cdd1ee280bb4c638005ffee0e
Peter Schoener <peter.c.schoener@gmail.com>
2017-06-06 13:08:52 +0200

    now reads and tokenizes files

--- PeterSchoenerAlonBorenstein.py
+++ PeterSchoenerAlonBorenstein.py
@@ -1,20 +1,15 @@
-# TO UPDATE FILE:
-# git commit -m "message"
-# git push
-# TO GET NEW VERSION
-# git pull
-
 import nltk
+
 #--------------Ex1------------------
-def tokenize(path):
-    sentences=[]
-    words=[]
-    file=open(path,"r",encoding="utf-8")
-    text=file.read().replace("\n", " ")
+def tokenize(path) :
+    sentences = []
+    words = []
+    file = open(path, "r", encoding = "utf-8")
+    text = file.read().replace("\n", " ")
     sentences = nltk.sent_tokenize(text)
     words = []
     for sentence in sentences :
         words.append(nltk.word_tokenize(sentence))
     print(words)
-tokenize("C:/Users/alon464/Desktop/assignment1-data/c00.txt")
-print("hi Alon")
+
+tokenize("./assignment1-data/c00.txt")
