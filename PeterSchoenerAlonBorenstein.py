2521bb542bcf7b7b6a99d6beb74af07392f4d722
Peter Schoener <peter.c.schoener@gmail.com>
2017-06-27 03:11:07 +0200

    finished implementing BackoffNgram

--- PeterSchoenerAlonBorenstein.py
+++ PeterSchoenerAlonBorenstein.py
@@ -17,9 +17,19 @@ def tokenize(path) :
 		words.append(nltk.word_tokenize(sentence))
 	return words
 
+# counterpart of sum, not in std lib
+def product(n_list) :
+	p = 1
+	for n in n_list :
+		p *= n
+	return p
+
 # represents an ngram model
 class Ngram :
 	def __init__(self, n) :
+		if n < 1 :
+			raise ValueError("n must be greater than 0.")
+	
 		# model is stored as a dictionary of dictionaries
 		# outer keys are the first n-1 elements of the ngram (the predictive part)
 		# inner keys are the last elements of the respective ngrams (the predicted part)
@@ -58,11 +68,11 @@ class Ngram :
 				self.__n_1 += 1
 			self.__model[key]["%%%TOTAL"] += 1
 		
-		self.__gt_discount = self.__n_1 / sum([r["%%%TOTAL"] for r in self.__model])
+		self.__gt_discount = self.__n_1 / sum([self.__model[r]["%%%TOTAL"] for r in self.__model])
 	
 	# helper method for prob_mle
 	# calculates log2 of the likelihood to prevent underflow
-	def __log_prob_mle(self, sentence) :
+	def log_prob_mle(self, sentence) :
 		# log2(probability of sentence) so far
 		p = 0
 		
@@ -80,11 +90,11 @@ class Ngram :
 	# takes a sentence as argument
 	# evaluates the likelihood (MLE) of the sentence against the model
 	def prob_mle(self, sentence) :
-		return math.pow(2, __log_prob_mle(sentence))
+		return math.pow(2, log_prob_mle(sentence))
 	
 	# helper method for prob_add
 	# calculates log2 of the likelihood to prevent underflow and to reduce operations in perplexity calculation
-	def __log_prob_add(self, sentence, alpha = 1) :
+	def log_prob_add(self, sentence, alpha = 1) :
 		if alpha <= 0 : # smoothing is only defined for positive alpha; else zero division could occur
 			raise ValueError("alpha must be positive.")
 	
@@ -112,24 +122,24 @@ class Ngram :
 	# optionally takes alpha as argument, defaults to 1 (Laplace)
 	# evaluates smoothed likelihood of the sentence against the model
 	def prob_add(self, sentence, alpha = 1) :
-		return pow(2, self.__log_prob_add(sentence, alpha))
+		return pow(2, self.log_prob_add(sentence, alpha))
 	
-	def __log_prob_gt(self, sentence) :
+	def log_prob_gt(self, sentence) :
 		p = 0
 		unresolved = []
 		
 		sentence = ["^^^"] + sentence + (["$$$"] * self.__n) # cf. update()
 		for i in range(self.__n, len(sentence)) :
 			key = tuple(sentence[i - self.__n : i])
-			if key in self.model and sentence[i] in self.model[key] :
-				p += math.log2(self.model[key][sentence[i]]) - math.log2(self.model[key]["%%%TOTAL"])
+			if key in self.__model and sentence[i] in self.__model[key] :
+				p += math.log2(self.__model[key][sentence[i]]) - math.log2(self.__model[key]["%%%TOTAL"])
 			else :
 				unresolved += [sentence[i + 1 - self.__n : i + 1]]
 		
 		return p, self.__gt_discount, unresolved
 	
 	def prob_gt(self, sentence) :
-		p, discount, unresolved = self.__log_prob_gt(sentence)
+		p, discount, unresolved = self.log_prob_gt(sentence)
 		return pow(2, p), discount, unresolved
 	
 	# takes a sentence as argument
@@ -139,7 +149,7 @@ class Ngram :
 		# calculate product of sentence likelihoods
 		p = 0
 		for sentence in sentences :
-			p += self.__log_prob_add(sentence, alpha)
+			p += self.log_prob_add(sentence, alpha)
 		
 		h = -1 * p / sum([len(s) for s in sentences])
 		return pow(2, h)
@@ -176,103 +186,133 @@ class Ngram :
 # represents an ngram model but with backoff probability estimation
 class BackoffNgram :
 	def __init__(self, n) :
+		if n < 1 :
+			raise ValueError("n must be greater than 0.")
+		
 		self.__ngram_models = [Ngram(x) for x in range(1, n + 1)]
 		self.__n = n
 	
 	def update(self, sentence) :
 		for model in self.__ngram_models :
 			model.update(sentence)
-		
-		for f_index in range(0, n - 1) :
-			
 	
-	def __log_prob(self, sentence, alpha) :
-		return 0
+	def log_prob(self, sentence, alpha) :
+		p = 0
+		unresolved = sentence
+		discounts = []
+		
+		for model in self.__ngram_models[:0:-1] : # this is different from [::-1] in that it removes the last element in the reversed array (unigrams should not be reduced)
+			ct = len(unresolved)
+			n_p, n_discount, unresolved = model.log_prob_gt(unresolved)
+			p += n_p
+			p += (math.log2(product([1 - d for d in discounts])) + math.log2(n_discount)) * ct
+			discounts += [n_discount]
+		
+		# last resort
+		ct = len(unresolved)
+		p += self.__ngram_models[0].log_prob_add(unresolved, alpha)
+		p += math.log2(product([1 - d for d in discounts])) * ct
+		
+		return p
 	
 	def prob(self, sentence, alpha = 1) :
-		return pow(2, self.__log_prob(sentence, alpha))
+		return pow(2, self.log_prob(sentence, alpha))
 	
 	def perplexity(self, sentences, alpha = 1) :
 		# calculate product of sentence likelihoods
 		p = 0
 		for sentence in sentences :
-			p += self.__log_prob(sentence, alpha)
+			p += self.log_prob(sentence, alpha)
 		
 		h = -1 * p / sum([len(s) for s in sentences])
 		return pow(2, h)
 
-# datasets
-test_set = []
-c_sentences = []
-d_sentences = []
-c_test = []
-d_test = []
+def __main__() :
+	# datasets
+	test_set = []
+	c_sentences = []
+	d_sentences = []
+	c_test = []
+	d_test = []
 
-# go through each file, tokenizing into appropriate dataset
-for file in os.listdir("./assignment1-data/") :
-	tokens = tokenize("./assignment1-data/" + file)
-	if file[0] == 'c' :
-		print(file)
-		if file[1:3] == "00" :
-			c_test += tokens
-		else :
-			c_sentences += tokens
-	elif file[0] == 'd' :
-		print(file)
-		if file[1:3] == "00" :
-			d_test += tokens
-		else :
-			d_sentences += tokens
-	elif file[0] == 't' :
-		print(file)
-		test_set += [tokens] # separate by document
+	# go through each file, tokenizing into appropriate dataset
+	for file in os.listdir("./assignment1-data/")[0:2] :
+		tokens = tokenize("./assignment1-data/" + file)
+		if file[0] == 'c' :
+			print(file)
+			if file[1:3] == "00" :
+				c_test += tokens
+			else :
+				c_sentences += tokens
+		elif file[0] == 'd' :
+			print(file)
+			if file[1:3] == "00" :
+				d_test += tokens
+			else :
+				d_sentences += tokens
+		elif file[0] == 't' :
+			print(file)
+			test_set += [tokens] # separate by document
 
-# make models: one each of order 1, 2, 3 with dataset c and d
-# populate models with appropriate ngrams
-models = [[Ngram(n) for n in range(1, 4)] for training_set in range(0, 2)]
-for order in range(1, 4) :
-	for sentence in c_sentences :
-		models[0][order - 1].update(sentence)
-	for sentence in d_sentences :
-		models[1][order - 1].update(sentence)
-
-# generate perplexity table (perplexity of each model on each test document)
-table = [["", "c1gl", "c1ga", "c2gl", "c2ga", "c3gl", "c3ga", "d1gl", "d1ga", "d2gl", "d2ga", "d3gl", "d3ga"]]
-alpha = 0.1
-
-# c00 validation row
-row = ["c00"]
-for training_set in range(0, 2) :
+	make models: one each of order 1, 2, 3 with dataset c and d
+	populate models with appropriate ngrams
+	models = [[Ngram(n) for n in range(1, 4)] for training_set in range(0, 2)]
 	for order in range(1, 4) :
-		print("est c validation", training_set, order)
-	#	alpha = models[training_set][order - 1].estimate_alpha(c_sentences)
-		row += [models[training_set][order - 1].perplexity(c_test)]
-		row += [models[training_set][order - 1].perplexity(c_test, alpha)]
-table += [row]
+		for sentence in c_sentences :
+			models[0][order - 1].update(sentence)
+		for sentence in d_sentences :
+			models[1][order - 1].update(sentence)
+	
+	# maybe works? takes ages though
+	# backoff_c = BackoffNgram(3)
+	# backoff_d = BackoffNgram(3)
+	# print(len(c_sentences))
+	# for sentence in c_sentences :
+		# print(sentence)
+		# backoff_c.update(sentence)
+	# for sentence in d_sentences :
+		# backoff_d.update(sentence)
+	# print(backoff_c.prob(["This", "is", "a", "test", "."]))
 
-# d00 validation row
-row = ["d00"]
-for training_set in range(0, 2) :
-	for order in range(1, 4) :
-		print("est d validation", training_set, order)
-	#	alpha = models[training_set][order - 1].estimate_alpha(d_sentences)
-		row += [models[training_set][order - 1].perplexity(d_test)]
-		row += [models[training_set][order - 1].perplexity(d_test, alpha)]
-table += [row]
+	generate perplexity table (perplexity of each model on each test document)
+	table = [["", "c1gl", "c1ga", "c2gl", "c2ga", "c3gl", "c3ga", "d1gl", "d1ga", "d2gl", "d2ga", "d3gl", "d3ga"]]
 
-# t documents
-t = 0;
-for test_document in test_set :
-	row = ["t0" + str(t)]
+	# c00 validation row
+	row = ["c00"]
 	for training_set in range(0, 2) :
 		for order in range(1, 4) :
-			print("est t", t, training_set, order)
-	#		alpha = models[training_set][order - 1].estimate_alpha(c_sentences + d_sentences)
-			row += [models[training_set][order - 1].perplexity(test_document)]
-			row += [models[training_set][order - 1].perplexity(test_document, alpha)]
+			print("est c validation", training_set, order)
+			alpha = models[training_set][order - 1].estimate_alpha(c_sentences)
+			row += [models[training_set][order - 1].perplexity(c_test)]
+			row += [models[training_set][order - 1].perplexity(c_test, alpha)]
 	table += [row]
-	t += 1
 
-# print table
-for row in table :
-	print(row)
+	# d00 validation row
+	row = ["d00"]
+	for training_set in range(0, 2) :
+		for order in range(1, 4) :
+			print("est d validation", training_set, order)
+			alpha = models[training_set][order - 1].estimate_alpha(d_sentences)
+			row += [models[training_set][order - 1].perplexity(d_test)]
+			row += [models[training_set][order - 1].perplexity(d_test, alpha)]
+	table += [row]
+
+	# t documents
+	t = 0;
+	for test_document in test_set :
+		row = ["t0" + str(t)]
+		for training_set in range(0, 2) :
+			for order in range(1, 4) :
+				print("est t", t, training_set, order)
+				alpha = models[training_set][order - 1].estimate_alpha(c_sentences + d_sentences)
+				row += [models[training_set][order - 1].perplexity(test_document)]
+				row += [models[training_set][order - 1].perplexity(test_document, alpha)]
+		table += [row]
+		t += 1
+
+	# print table
+	for row in table :
+		print(row)
+
+if __name__ == "__main__" :
+	__main__()
